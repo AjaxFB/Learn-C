632ba5a17dd1786aebfb943ec94d10970057f8d2
AjaxFB <bruno.damato95@hotmail.com>
2017-02-07 16:06:47 -0200

    Error find on operator_all

--- K&R/rpn/solution.c
+++ K&R/rpn/solution.c
@@ -1,13 +1,63 @@
 // Copyright 2014 Bruno D'Amato <?>
 //libs
 #include<stdio.h>
-  main() {
+
     //inicialize variables
     int stack[10];
     int pointer = 0;
     char in ;
     int trash, operand;
     int length = sizeof(stack) / sizeof(stack[0]);
+
+void operator_all(char in_c){
+for (; pointer > 1; pointer--) {
+	if(in_c == '+'){
+		//problem on this line
+		//printf("%i += %i\n",stack[pointer - 2],stack[pointer - 1]);
+		//stack[pointer - 2] = stack[pointer - 2] + stack[pointer - 1]; //dont use =+
+		stack[pointer - 2] += stack[pointer - 1]; //dont use =+
+	}else{
+		if(in_c == '-'){
+			stack[pointer - 2] -= stack[pointer - 1]; //dont use =+
+		}else{
+				if(in_c == '*'){
+					stack[pointer - 2] *= stack[pointer - 1]; //dont use =+
+				}else{
+					stack[pointer - 2] /= stack[pointer - 1]; //dont use =+
+				}
+		}
+	}
+    stack[pointer - 2] -= stack[pointer - 1]; //dont use =+
+    stack[pointer - 1] = 0;
+    pointer--;
+}
+pointer = 1;
+
+
+}
+
+void operand_all(char in_o){
+    for (; operand > 1; operand--) {
+	if(in_o == '+'){
+		stack[pointer - 2] += stack[pointer - 1]; //dont use =+
+	}else{
+		if(in_o == '-'){
+			stack[pointer - 2] -= stack[pointer - 1]; //dont use =+
+		}else{
+				if(in_o == '*'){
+					stack[pointer - 2] *= stack[pointer - 1]; //dont use =+
+				}else{
+					stack[pointer - 2] /= stack[pointer - 1]; //dont use =+
+				}
+		}
+	}
+    stack[pointer - 1] = 0;
+    pointer--;
+    }
+}
+
+int main() {
+
 
     for (trash = 0; trash < 10; trash++) {
       stack[trash] = 0;
@@ -21,6 +71,7 @@
         stack[pointer] = (int) in -48; //atoi and (int) dont convert
         operand++;
         pointer++;
+	printf("number:pointer: %i operator: %i\n", pointer,operand);
       } else {
         if ( in == '+' || in == '-' || in == '*' || in == '/') {
           /*
@@ -30,76 +81,36 @@
           */
           if ( in == '+') {
             if (operand == 0) {
-              for (; pointer > 1; pointer--) {
-                stack[pointer - 2] += stack[pointer - 1]; //dont use =+
-                stack[pointer - 1] = 0;
-                pointer--;
-              }
+				operator_all(in);
             } else {
-              for (; operand > 1; operand--) {
-                //printf("%i += %i\n",stack[pointer - 2],stack[pointer - 1]);
-                stack[pointer - 2] += stack[pointer - 1]; //dont use =+
-                stack[pointer - 1] = 0;
-                pointer--;
-              }
-              
+              operand_all(in);
             }
           }
           if ( in == '*') {
             if (operand == 0) {
-              for (; pointer > 1; pointer--) {
-                stack[pointer - 2] *= stack[pointer - 1]; //dont use =+
-                stack[pointer - 1] = 0;
-                pointer--;
-              }
+				operator_all(in);
             } else {
-              for (; operand > 1; operand--) {
-                //printf("%i += %i\n",stack[pointer - 2],stack[pointer - 1]);
-                stack[pointer - 2] *= stack[pointer - 1]; //dont use =+
-                stack[pointer - 1] = 0;
-                pointer--;
-              }
-              
+              operand_all(in);
             }
           }
           if ( in == '/') {
             if (operand == 0) {
-              for (; pointer > 1; pointer--) {
-                stack[pointer - 2] /= stack[pointer - 1]; //dont use =+
-                stack[pointer - 1] = 0;
-                pointer--;
-              }
+				operator_all(in);
             } else {
               for (; operand > 1; operand--) {
-                //printf("%i += %i\n",stack[pointer - 2],stack[pointer - 1]);
-                stack[pointer - 2] /= stack[pointer - 1]; //dont use =+
-                stack[pointer - 1] = 0;
-                pointer--;
+                operand_all(in);
               }
-              
             }
           }
           if ( in == '-') {
             if (operand == 0) {
-              for (; pointer > 1; pointer--) {
-                stack[pointer - 2] -= stack[pointer - 1]; //dont use =+
-                stack[pointer - 1] = 0;
-                pointer--;
-              }
+				operator_all(in);
             } else {
-              for (; operand > 1; operand--) {
-                //printf("%i += %i\n",stack[pointer - 2],stack[pointer - 1]);
-                stack[pointer - 2] -= stack[pointer - 1]; //dont use =+
-                stack[pointer - 1] = 0;
-                pointer--;
-              }
-              
+				operand_all(in);
             }
           }
-
-          printf("value:\t%i\n", stack[pointer]);
-
           operand = 0;
+          printf("operator:pointer: %i operator: %i\n", pointer,operand);
         } else {
           if ( in == '\n') {
             //pop and print top of stack
@@ -107,6 +118,7 @@
               printf("%i\t", stack[trash]);
             }
             printf("\n");
+			printf("line break:pointer: %i operator: %i\n", pointer,operand);
           } else {
             printf("Error.\n");
           }
